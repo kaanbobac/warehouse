d2bcdeb78b26748039a690d02b77d0144c87fce3
Kaan Bobac <bobackaan@gmail.com>
2021-04-27 08:44:04 +0300

    added e2e unit test

---
+++ src/test/java/kaan/demo/warehouse/e2e/QueryAllProductsAvailableTest.java
@@ -0,0 +1,68 @@
+package kaan.demo.warehouse.e2e;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kaan.demo.warehouse.dto.ProductJsonDto;
+import kaan.demo.warehouse.model.Product;
+import kaan.demo.warehouse.repo.ProductRepository;
+
+@SpringBootTest(properties = { "app.data.inventory.json.path=/test/json/test04-inventory.json",
+		"app.data.product.json.path=/test/json/test04-products.json" })
+@AutoConfigureMockMvc
+public class QueryAllProductsAvailableTest {
+	@Autowired
+	private MockMvc mvc;
+	private static List<ProductJsonDto> products = new ArrayList<>();
+	private static String jsonResponse;
+	@Autowired
+	private ProductRepository productRepo;
+
+	public void init() {
+		List<Product> database = (List<Product>) productRepo.findAll();
+		for (Product p : database) {
+			ProductJsonDto product = new ProductJsonDto();
+			String name = p.getName();
+			product.setName(p.getName());
+			product.setId(p.getId());
+			if (name.equals("Dining Chair")) {
+				product.setQuantity(2);
+			} else if (name.equals("Dining Table")) {
+				product.setQuantity(3);
+			} else if (name.equals("New Product")) {
+				product.setQuantity(0);
+			}
+			products.add(product);
+		}
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			jsonResponse = objectMapper.writeValueAsString(products);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void queryAll() throws Exception {
+		init();
+		mvc.perform(get("/query-all").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(content().json(jsonResponse));
+
+	}
+}
